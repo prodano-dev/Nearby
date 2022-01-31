627b20585cf0c6dd0b5329af662961a755c5bb76
Danny Pro <danny.pro17@gmail.com>
2022-01-31 18:20:08 +0100

    first commit

---
+++ Nearby/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//
+// ContentView.swift
+// Nearby
+//
+// Created by prodano.dev on 31/01/2022
+// Copyright © 2022 prodano.dev. All rights reserved.
+//
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nearby/NearbyApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// NearbyApp.swift
+// Nearby
+//
+// Created by prodano.dev on 31/01/2022
+// Copyright © 2022 prodano.dev. All rights reserved.
+//
+        
+
+import SwiftUI
+
+@main
+struct NearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
